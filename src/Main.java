14a3a08c80315c6a23beda5be58c608f0a082b7a
Daniela <danielagp96@gmail.com>
2023-04-12 16:16:56 -0600

    clearing main

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,7 @@
-import static ui.UIMenu.*;//import static ant * so I can use all the public static methods of UIMenu
+import static ui.UIMenu.*;
 
-// Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
-        myDoctor.name="Daniela Gutierrez";
-        myDoctor.showName();
-        myDoctor.showId();
-
-        Doctor myDoctor2 = new Doctor();
-        myDoctor2.showId();
-
-        //I can increment this static value and the next doctor will be id 4 this can show the global scope
-        Doctor.id++;
-        System.out.println("incrementing id everywhere: "+Doctor.id);
-
-        Doctor myDoctor3 = new Doctor();
-        myDoctor3.showId();
-        showMenu();//I do not need to use the class name because of the import type I used
+        showMenu();
     }
 }
