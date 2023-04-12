0650bddf471656a8e6210490df268f61a62194bc
Daniela <danielagp96@gmail.com>
2023-04-12 16:28:19 -0600

    adding MONTHS to the menu option book an appoiment

--- src/ui/UIMenu.java
+++ src/ui/UIMenu.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class UIMenu {
     //this method is static because conceptually it is not correct to create an object of UIMenu to use its methods, so I put this class on a package to be able to import on this format import static ui.UIMenu.* in this way I do not need to use the name of the class to invoke static methods
+    static final String[] MONTHS={"enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"}; //months never will change so final is used for constans it is not necesary to use it with static but most times it will happen and constans always will be in capital letters
     public static void showMenu(){
         System.out.println("Welcome to My Appointments");
         System.out.println("Selecciona la opción deseada");
@@ -50,6 +51,9 @@ public class UIMenu {
             switch (response){
                 case 1:
                     System.out.println("::Book an appointment");
+                    for (int i = 0; i < 3; i++) {
+                        System.out.println(i+" ."+MONTHS[i]);
+                    }
                     break;
                 case 2:
                     System.out.println("::My appointments");
