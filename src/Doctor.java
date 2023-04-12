94da5a199548bfb6c37e640b4ea21597aca2051a
Daniela <danielagp96@gmail.com>
2023-04-12 15:10:22 -0600

    adding two constructors

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,7 +1,12 @@
 public class Doctor {
     int id;
     String name;
-
+    Doctor(){
+        System.out.println("ctor Doctor");
+    }
+    Doctor(String pName){
+        System.out.println("ctor Doctor with name:"+pName);
+    }
     //behaviors
     public void showName(){
         System.out.println(name);
