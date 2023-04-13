ec8baa432c38c0789727155c9b3cf9ed784f231a
Daniela <danielagp96@gmail.com>
2023-04-13 09:12:07 -0600

    anonymous classes and interfaces example

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import model.Doctor;
+import model.IScheduable;
 import model.Patient;
+import model.User;
 
 import java.util.Date;
 
@@ -19,5 +21,19 @@ public class Main {
         Patient p1= new Patient("sofi","sofi@gmail.com");
         //here I can see the effect of use toString override
         System.out.println(p1);
+
+        User u1 = new User("dani","dani¡gmail.com") {//this is a way to create an instance of an abstract class and is called anonymous classes  but their life cycle is only At this moment
+            @Override
+            public String showDataUser() {
+                return null;
+            }
+        };
+
+        IScheduable scheduable = new IScheduable() {//we can do the same with interfaces this is usually used on android to handle events
+            @Override
+            public void schedule(Date date, String time) {
+
+            }
+        };
     }
 }
