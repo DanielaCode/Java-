791ecae1b75414a68384dbccf9f307af2e1d6d37
Daniela <danielagp96@gmail.com>
2023-04-12 18:26:00 -0600

    nested class implementation and explanation with an example available appointments for doctors only requirement

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Doctor {
@@ -6,22 +7,50 @@ public class Doctor {
     private String email;
     private String speciality;
 
-    /*I need to put available appointments for doctors, so I create this:*/
-    int idAvailableAppointments;
-    private Date date;
-    private String time;
-    /*and then create a collection to list all the agenda*/
-
+    ArrayList<AvailableAppointments> aA = new ArrayList<>();
     Doctor(String pName, String pSpeciality){
         id++;
         this.name = pName;
         this.speciality = pSpeciality;
     }
     //behaviors
+    public void addAvailableApointments(Date date, String time){
+        aA.add(new AvailableAppointments(date,time));
+    }
+    public ArrayList<AvailableAppointments> getAvailableApointments(){
+        return aA;
+    }
     public void showName(){
         System.out.println(name);
     }
     public void showId(){
         System.out.println("Id doctor:"+id);
+    }
+    static public class AvailableAppointments{
+        /*because I needed to create a class to make a collection of this the best solution was to create a class but because this class only will be used by this class doctor the best solution is a nested class*/
+        int idAvailableAppointments;
+        private Date date;
+        private String time;
+
+        public AvailableAppointments(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
         //showMenu();
         Doctor d1= new Doctor("Daniela","pediatry");
+        //nested class justification: only doctor class can use it
+        d1.addAvailableApointments(new Date(),"4pm");
+        d1.addAvailableApointments(new Date(),"6pm");
+        d1.addAvailableApointments(new Date(),"3pm");
 
+        for (Doctor.AvailableAppointments aA: d1.getAvailableApointments()) {
+            System.out.println("date: "+aA.getDate()+"time: "+aA.getTime());
+        }
     }
 }
